e537d301e49555f840dbbfdd2b3904fa7c8c1935
Subham Bansal <subham.bansal@housing.com>
2015-06-30 13:44:11 +0530

    Sliding Ui for android...Pending: adding listview and binding

---
+++ SlidingMenu/app/src/main/java/info/androidhive/slidingmenu/HomeFragment.java
@@ -0,0 +1,121 @@
+package info.androidhive.slidingmenu;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonArrayRequest;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import info.androidhive.slidingmenu.app.AppController;
+import info.androidhive.slidingmenu.model.Movie;
+
+import info.androidhive.slidingmenu.adapter.CustomListAdapter;
+
+public class HomeFragment extends Fragment {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    // Movies json url
+    private static final String url = "http://api.androidhive.info/json/movies.json";
+    private ProgressDialog pDialog;
+    private List<Movie> movieList = new ArrayList<Movie>();
+    private ListView listView;
+    private CustomListAdapter adapter;
+
+    public HomeFragment(){}
+	
+	@Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+ 
+        View rootView = inflater.inflate(R.layout.fragment_home, container, false);
+
+        //listView = (ListView) rootView.findViewById(R.id.list);
+        //adapter = new CustomListAdapter(this, movieList);
+        //listView.setAdapter(adapter);
+
+        /*pDialog = new ProgressDialog(this.getActivity());
+        // Showing progress dialog before making http request
+        pDialog.setMessage("Loading...");
+        pDialog.show();
+
+
+        // Creating volley request obj
+        /*JsonArrayRequest movieReq = new JsonArrayRequest(url,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        Log.d(TAG, response.toString());
+                        hidePDialog();
+
+                        // Parsing json
+                        for (int i = 0; i < response.length(); i++) {
+                            try {
+
+                                JSONObject obj = response.getJSONObject(i);
+                                Movie movie = new Movie();
+                                movie.setTitle(obj.getString("title"));
+                                movie.setThumbnailUrl(obj.getString("image"));
+                                movie.setRating(((Number) obj.get("rating"))
+                                        .doubleValue());
+                                movie.setYear(obj.getInt("releaseYear"));
+
+                                // Genre is json array
+                                JSONArray genreArry = obj.getJSONArray("genre");
+                                ArrayList<String> genre = new ArrayList<String>();
+                                for (int j = 0; j < genreArry.length(); j++) {
+                                    genre.add((String) genreArry.get(j));
+                                }
+                                movie.setGenre(genre);
+
+                                // adding movie to movies array
+                                movieList.add(movie);
+
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+
+                        }
+
+                        // notifying list adapter about data changes
+                        // so that it renders the list view with updated data
+                        adapter.notifyDataSetChanged();
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                VolleyLog.d(TAG, "Error: " + error.getMessage());
+                hidePDialog();
+
+            }
+        });
+
+        // Adding request to request queue
+        AppController.getInstance().addToRequestQueue(movieReq);*/
+         
+        return rootView;
+    }
+
+    public void hidePDialog() {
+        if (pDialog != null) {
+            pDialog.dismiss();
+            pDialog = null;
+        }
+    }
+}
---
+++ SlidingMenu/app/src/main/java/info/androidhive/slidingmenu/adapter/CustomListAdapter.java
@@ -0,0 +1,92 @@
+package info.androidhive.slidingmenu.adapter;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+import android.app.Fragment;
+
+import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.NetworkImageView;
+
+import java.util.List;
+
+import info.androidhive.slidingmenu.R;
+import info.androidhive.slidingmenu.app.AppController;
+import info.androidhive.slidingmenu.model.Movie;
+
+public class CustomListAdapter extends BaseAdapter {
+	private Fragment fragment;
+	private LayoutInflater inflater;
+	private List<Movie> movieItems;
+	ImageLoader imageLoader = AppController.getInstance().getImageLoader();
+
+	public CustomListAdapter(Fragment fragment, List<Movie> movieItems) {
+		this.fragment = fragment;
+		this.movieItems = movieItems;
+	}
+
+	@Override
+	public int getCount() {
+		return movieItems.size();
+	}
+
+	@Override
+	public Object getItem(int location) {
+		return movieItems.get(location);
+	}
+
+	@Override
+	public long getItemId(int position) {
+		return position;
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+
+		if (inflater == null)
+			inflater = (LayoutInflater) fragment.getActivity()
+					.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		if (convertView == null)
+			convertView = inflater.inflate(R.layout.list_row, null);
+
+		if (imageLoader == null)
+			imageLoader = AppController.getInstance().getImageLoader();
+		NetworkImageView thumbNail = (NetworkImageView) convertView
+				.findViewById(R.id.thumbnail);
+		TextView title = (TextView) convertView.findViewById(R.id.title);
+		TextView rating = (TextView) convertView.findViewById(R.id.rating);
+		TextView genre = (TextView) convertView.findViewById(R.id.genre);
+		TextView year = (TextView) convertView.findViewById(R.id.releaseYear);
+
+		// getting movie data for the row
+		Movie m = movieItems.get(position);
+
+		// thumbnail image
+		thumbNail.setImageUrl(m.getThumbnailUrl(), imageLoader);
+		
+		// title
+		title.setText(m.getTitle());
+		
+		// rating
+		rating.setText("Rating: " + String.valueOf(m.getRating()));
+		
+		// genre
+		String genreStr = "";
+		for (String str : m.getGenre()) {
+			genreStr += str + ", ";
+		}
+		genreStr = genreStr.length() > 0 ? genreStr.substring(0,
+				genreStr.length() - 2) : genreStr;
+		genre.setText(genreStr);
+		
+		// release year
+		year.setText(String.valueOf(m.getYear()));
+
+		return convertView;
+	}
+
+}
