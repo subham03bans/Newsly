62f5781f01fc454cd2649ae22c5a013084a95c30
Aakash Upadhyay <Aakash@Aakashs-MacBook-Pro.local>
2015-06-30 15:45:09 +0530

    Google Authentication

---
+++ blog/create_news_database/app/controllers/news_controller.rb
@@ -0,0 +1,42 @@
+class NewsController < ApplicationController
+	skip_before_filter  :verify_authenticity_token
+  def index
+  	@all_news = News.all
+  end
+  def sports
+  	@all_news = News.select("*").where(:category => "sports")
+  	render "index"
+  	#redirect_to :action => :'index'
+  end
+  def business
+  	@all_news = News.select("*").where(:category => "business")
+  	render "index"
+  end
+  def technology
+  	@all_news = News.select("*").where(:category => "tech")
+  	render "index"
+  end
+  def entertainment
+  	@all_news = News.select("*").where(:category => "entertainment")
+  	render "index"
+  end
+def search
+    if params[:q].nil? or params[:q].empty?
+      @all_news = News.all
+      puts 'nilllllllllll'
+    else
+      @all_news= News.search params[:q]
+      puts 'not nilllllllllll'
+      #puts @all_news
+    end
+      render "index"
+  end
+
+  def create
+  	if !News.exists?(title: params[:title])
+	  	News.new(:category => params[:category], :title => params[:title], :content => params[:content], :url => params[:url]).save
+	end
+	redirect_to :action => :'index'
+  end
+
+end
