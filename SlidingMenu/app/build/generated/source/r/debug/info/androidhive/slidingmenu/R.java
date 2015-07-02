ae73d3250855b1bc72e27e42e809f7647b366d76
Subham Bansal <subham.bansal@housing.com>
2015-07-02 10:54:30 +0530

    Improved view

--- SlidingMenu/app/build/generated/source/r/debug/info/androidhive/slidingmenu/R.java
+++ SlidingMenu/app/build/generated/source/r/debug/info/androidhive/slidingmenu/R.java
@@ -17,18 +17,25 @@ public final class R {
     public static final class color {
         public static final int action_bar_color=0x7f070000;
         public static final int blue_color=0x7f070001;
-        public static final int counter_text_bg=0x7f070002;
-        public static final int counter_text_color=0x7f070003;
-        public static final int green_color=0x7f070004;
-        public static final int list_background=0x7f070005;
-        public static final int list_background_pressed=0x7f070006;
-        public static final int list_divider=0x7f070007;
-        public static final int list_item_title=0x7f070008;
-        public static final int list_row_end_color=0x7f070009;
-        public static final int list_row_hover_end_color=0x7f07000a;
-        public static final int list_row_hover_start_color=0x7f07000b;
-        public static final int list_row_start_color=0x7f07000c;
-        public static final int red_color=0x7f07000d;
+        public static final int business_cat_color=0x7f070002;
+        public static final int counter_text_bg=0x7f070003;
+        public static final int counter_text_color=0x7f070004;
+        public static final int entertainment_cat_color=0x7f070005;
+        public static final int green_color=0x7f070006;
+        public static final int headline_color=0x7f070007;
+        public static final int india_cat_color=0x7f070008;
+        public static final int list_background=0x7f070009;
+        public static final int list_background_pressed=0x7f07000a;
+        public static final int list_divider=0x7f07000b;
+        public static final int list_item_title=0x7f07000c;
+        public static final int list_row_end_color=0x7f07000d;
+        public static final int list_row_hover_end_color=0x7f07000e;
+        public static final int list_row_hover_start_color=0x7f07000f;
+        public static final int list_row_start_color=0x7f070010;
+        public static final int red_color=0x7f070011;
+        public static final int sports_cat_color=0x7f070012;
+        public static final int technology_cat_color=0x7f070013;
+        public static final int world_cat_color=0x7f070014;
     }
     public static final class dimen {
         public static final int activity_horizontal_margin=0x7f040000;
@@ -42,45 +49,48 @@ public final class R {
     }
     public static final class drawable {
         public static final int counter_bg=0x7f020000;
-        public static final int ic_communities=0x7f020001;
-        public static final int ic_drawer=0x7f020002;
-        public static final int ic_home=0x7f020003;
-        public static final int ic_launcher=0x7f020004;
-        public static final int ic_pages=0x7f020005;
-        public static final int ic_people=0x7f020006;
-        public static final int ic_photos=0x7f020007;
-        public static final int ic_whats_hot=0x7f020008;
-        public static final int list_item_bg_normal=0x7f020009;
-        public static final int list_item_bg_pressed=0x7f02000a;
-        public static final int list_row_bg=0x7f02000b;
-        public static final int list_row_bg_hover=0x7f02000c;
-        public static final int list_row_selector=0x7f02000d;
-        public static final int list_selector=0x7f02000e;
+        public static final int ic_action_search=0x7f020001;
+        public static final int ic_communities=0x7f020002;
+        public static final int ic_drawer=0x7f020003;
+        public static final int ic_home=0x7f020004;
+        public static final int ic_launcher=0x7f020005;
+        public static final int ic_pages=0x7f020006;
+        public static final int ic_people=0x7f020007;
+        public static final int ic_photos=0x7f020008;
+        public static final int ic_whats_hot=0x7f020009;
+        public static final int list_item_bg_normal=0x7f02000a;
+        public static final int list_item_bg_pressed=0x7f02000b;
+        public static final int list_row_bg=0x7f02000c;
+        public static final int list_row_bg_hover=0x7f02000d;
+        public static final int list_row_selector=0x7f02000e;
+        public static final int list_selector=0x7f02000f;
     }
     public static final class id {
-        public static final int action_settings=0x7f0a0016;
-        public static final int category=0x7f0a000f;
+        public static final int action_search=0x7f0a0017;
+        public static final int action_settings=0x7f0a0018;
+        public static final int category=0x7f0a0010;
         public static final int category_detailed_view=0x7f0a0006;
         public static final int contents_detailed_view=0x7f0a0007;
         public static final int counter=0x7f0a000a;
-        public static final int downvotes=0x7f0a0015;
-        public static final int downvotes_image=0x7f0a0014;
+        public static final int downvotes=0x7f0a0016;
+        public static final int downvotes_image=0x7f0a0015;
         public static final int drawer_layout=0x7f0a0000;
         public static final int frame_container=0x7f0a0001;
-        public static final int headline=0x7f0a000e;
+        public static final int headline=0x7f0a000f;
         public static final int headline_detailed_view=0x7f0a0005;
         public static final int icon=0x7f0a0008;
         public static final int list=0x7f0a000c;
+        public static final int list_row_content_layout=0x7f0a000e;
         public static final int list_slidermenu=0x7f0a0002;
-        public static final int placeholder=0x7f0a0010;
-        public static final int publisher_name=0x7f0a0011;
+        public static final int placeholder=0x7f0a0011;
+        public static final int pub_time=0x7f0a0012;
         public static final int scrollview1=0x7f0a0003;
         public static final int thumbnail=0x7f0a000d;
         public static final int thumbnail_detailed_view=0x7f0a0004;
         public static final int title=0x7f0a0009;
         public static final int txtLabel=0x7f0a000b;
-        public static final int upvotes=0x7f0a0013;
-        public static final int upvotes_image=0x7f0a0012;
+        public static final int upvotes=0x7f0a0014;
+        public static final int upvotes_image=0x7f0a0013;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -100,16 +110,17 @@ public final class R {
         public static final int menu_main_activity2=0x7f090001;
     }
     public static final class string {
-        public static final int action_settings=0x7f080000;
-        public static final int app_name=0x7f080001;
-        public static final int desc_list_item_icon=0x7f080002;
-        public static final int drawer_close=0x7f080003;
-        public static final int drawer_open=0x7f080004;
-        public static final int hello_blank_fragment=0x7f080005;
-        public static final int hello_world=0x7f080006;
-        public static final int title_activity_main_activity2=0x7f080007;
-        public static final int try_url=0x7f080008;
-        public static final int vertical_bar_char=0x7f080009;
+        public static final int action_search=0x7f080000;
+        public static final int action_settings=0x7f080001;
+        public static final int app_name=0x7f080002;
+        public static final int desc_list_item_icon=0x7f080003;
+        public static final int drawer_close=0x7f080004;
+        public static final int drawer_open=0x7f080005;
+        public static final int hello_blank_fragment=0x7f080006;
+        public static final int hello_world=0x7f080007;
+        public static final int title_activity_main_activity2=0x7f080008;
+        public static final int try_url=0x7f080009;
+        public static final int vertical_bar_char=0x7f08000a;
     }
     public static final class style {
         /**  API 11 theme customizations can go here. 
